796a4dfefcf32fb1813f7c442a5396b69e148cac
Corina Talavera <corina.talavera@gmail.com>
2018-11-10 20:29:16 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+  def name=(name)
+    @name="Beyonce"
+  end
+ 
+  def name
+    name="Beyonce"
+  end
+  def job=(job)
+    @job="Beagle"
+  end
+ 
+  def breed
+    name="Beagle"
+  end
+end
