476c074b9c72399d4695e7d2b9470ce6a751a90b
Corina Talavera <corina.talavera@gmail.com>
2018-11-10 20:22:31 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
- fido = Dog.new
- fido.name = "Fido"
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
+end
+ 
+fido = Dog.new
+fido.name = "Fido"
+ 
+puts fido.name
